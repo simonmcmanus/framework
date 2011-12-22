9fb1f2a58807d2d8f8a3a5ac08de315cd8fbdee3
Simon McManus <mcmanus.simon@gmail.com>
2011-12-22 22:16:40 +0000

    animation position working on photo view

--- views/photo/photo.js
+++ views/photo/photo.js
@@ -33,34 +33,31 @@ structure.views.photo = function(domNode) {
 	
 	that.show = function(options, callback) {
 		var $clicked = $(options.clicked);
-		that.domNode.find('h3').hide().fadeIn('slow');
 		that.domNode.find('#photo img').bind('load', function() {
 			var h = $(this).height();
 			var w = $(this).width();
-			var t = $(this).position().top;	 
-			var l = $(this).position().left;
+			var t = $(this).position().top + $('#container').offset().top;
+			var l = $(this).offset().left + 9999 + $('#container').position().left;
+//			debugger;
+			that.domNode.find('h3').hide().fadeIn('slow');
 			console.log(h,w, t, l);
 			$(this).css({
 				height:'75',
 				width: 75,
 				left: $clicked.offset().left, 
 				top: $clicked.offset().top,
-				position: 'absolute',
-				border: '1px solid red'
+				position: 'absolute'
 			});
-			$('#container .active').removeClass('active').addClass('inactive');			
+			
+			$('#container .active').removeClass('active').addClass('inactive');
 			that.domNode.addClass('active').removeClass('inactive');
-			
-			
-			$(this).attr('start-height', h);
-			$(this).attr('start-width', w);
 			
 			
 			$(this).animate({
 				width: w,
 				height: h,
-				top: $('#container').position().top+60,
-				left: $('#container').position().left
+				top: t,
+				left: l
 			}, 1000, function() {		
 				$(this).css('position', 'static');
 				
